34b04a6f9122ecec9e13179b3cf9a0b0bb75efd6
sunil <sunil@codebee.dk>
2021-06-16 09:26:58 +0545

    GIP initial console app

---
+++ GeoIPTrack/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoIPTrack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console app to get ip");
+            // to hold screen 
+            Console.ReadKey();
+        }
+    }
+}
